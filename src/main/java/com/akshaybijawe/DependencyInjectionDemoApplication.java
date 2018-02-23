497bb7b7eaf150fc48cec0a99bcfdced726ef7ba
Akshay Bijawe <akshaybijawe121@gmail.com>
2018-02-23 15:13:45 -0800

    Add the test controller

--- src/main/java/com/akshaybijawe/DependencyInjectionDemoApplication.java
+++ src/main/java/com/akshaybijawe/DependencyInjectionDemoApplication.java
@@ -1,12 +1,20 @@
 package com.akshaybijawe;
 
+import com.akshaybijawe.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
+
 	}
 }
